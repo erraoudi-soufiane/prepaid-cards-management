fef10375992c06a38c94f427f6d8cdedc986fa19
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-11-27 15:30:37 +0100

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Hello</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+      height="100vh"
+      templateRows="auto 1fr"
+    >
+      <GridItem area="nav" height="100%">
+        Nav
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside" height="100%">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="#F5F5F5" padding={3} flex="1" height="1fr">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
